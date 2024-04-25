d56817142fe8aa1f5ccd53b7364666d4f5e04375
ChristianBernhard <christian.bernhard97@web.de>
2024-04-25 15:15:05 +0200

    clean up

---
+++ evaluation.py
@@ -0,0 +1,8 @@
+# TODO
+# create some looping code here to:
+# - comparison cpu vs gpu
+# - run 4bit, 8bit, 32bit
+# - compare llama.cpp vs. own implementation
+# - compare token/s, firsttoken_duration, avg_token_time
+# - compare output mit augenmaß
+# - compare different inference strats
